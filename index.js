2e5ceb870bd2f83979dbbba02e794f76fa999a48
github username <github email address>
2019-08-12 08:32:57 -0400

    Done.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
   let joke;
 
   function fetchJoke(){
-    fetch('https://icanhazdadjoke.com/', {
+    return fetch('https://icanhazdadjoke.com/', {
       headers: {
         "Accept": "application/json"
       }
@@ -14,14 +14,19 @@ document.addEventListener('DOMContentLoaded', () => {
     .then(res => res.json())
     .then(jokeData => joke = jokeData.joke)
   }
-
+  function setJoke(){
+    fetchJoke()
+  }
+  setJoke()
   form.addEventListener('submit', (event) => {
-
+    event.preventDefault()
+    const username = event.target[0].value
     if(username === "") return;
     fetchJoke()
     newJokeLi.innerHTML = `
     <span class="username">${username} says:</span> ${joke}
-    `
+    `    
     jokeList.appendChild(newJokeLi)
+    setJoke()
   })
 })
